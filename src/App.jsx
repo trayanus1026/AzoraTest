1fa9e30938d69bb7e56377acb115b99b0a161f38
trayanus1026 <trayanus929@gmail.com>
2026-01-13 02:47:09 +0100

    feat: header link complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen w-full text-white font-['Avenir Next']">
+      <div className="flex h-[40px] mt-[17px] px-[40px] flex items-center">
+        <div className="flex justify-start">
+          <img src="/assets/SVG.svg" alt="brand" />
+        </div>
+        <div className="flex justify-end ml-auto">
+          <div className="flex gap-[30px] font-medium text-sm leading-[25.76px] tracking-[0.9px] items-center">
+            <a href="#" className="no-underline ">How it works</a>
+            <a href="#" className="no-underline ">All apps</a>
+            <a href="#" className="no-underline ">Pricing</a>
+            <a href="#" className="no-underline ">For Teams</a>
+            <a href="#" className="no-underline ">Blog</a>
+            <a href="#" className="no-underline ">Podcast</a>
+            <span className="">|</span>
+            <img src="/assets/en.svg" alt="english" />
+            <a href="#" className="no-underline ">Sign in</a>
+            <button className="border rounded-md font-normal text-sm leading-[20px] tracking-[1px] pt-[5.5px] pb-[6.5px] px-[23px] cursor-pointer">
+              Try free
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
