1dffebf38a2fecce8514c2222fba4e14d8451d09
trayanus1026 <trayanus929@gmail.com>
2026-01-13 06:08:01 +0100

    feat: add second section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div className="min-h-screen w-full text-white font-['Avenir Next']">
+    <div className="min-h-screen w-full max-w-[1440px] mx-auto text-white font-['Avenir Next']">
       {/* Start Header */}
-      <div className="flex h-[40px] mt-[17px] px-[40px] flex items-center">
+      <div className="flex h-[40px] mt-[17px] px-[40px] items-center">
         <div className="flex justify-start">
           <img src="/assets/SVG.svg" alt="brand" />
         </div>
@@ -26,7 +26,7 @@ function App() {
       {/* End Header */}
 
       {/* Start section1 */}
-      <div className="flex h-[595.59px] mt-[118px] pl-[80px] pr-[60px] justify-between">
+      <div className="flex h-[595.59px] mt-[118px] pl-[80px] pr-[60px] justify-between mb-[130px]">
         <div>
           <div className="relative mt-[50px]">
             <img src="/assets/pics.svg" alt="pics" className="w-[173px] h-[173px]" />
@@ -65,6 +65,53 @@ function App() {
         </div>
       </div>
       {/* End section1 */}
+
+      {/* Start section2 */}
+      <div className="flex flex-col mx-[130px] border-t border-t-[#e5e5e5] border-b border-b-[#e5e5e5]">
+        <div className="flex h-[71.84px] w-full mt-[101px]">
+          <span className="w-1/2 font-semibold text-4xl leading-[51.84px] tracking-[1px]">What you get on Setapp.</span>
+          <span className="w-1/2 font-normal text-lg leading-[32.04px] tracking-normal">With a single monthly subscription at $9.99, you get 240+ apps for<br />your Mac.</span>
+        </div>
+        <div className="mt-[87.99px] bg-[#DF96AE] rounded-[20px] h-[605px] w-full">
+          <div className="flex mt-[60px] ml-[60px]">
+            <img src="/assets/icon-cmm.png" alt="icon-cmm" className="w-[80px] h-[80px]" />
+            <div className="flex flex-col ml-[32px]">
+              <span className="font-semibold text-[26px] leading-[40.04px] tracking-normal text-[#26262B]">Keep your Mac clean</span>
+              <span className="font-normal text-lg leading-[32.04px] tracking-normal text-[#26262B]">Remove junk, scan for malware, wipe email attachments</span>
+            </div>
+          </div>
+          <img src="/assets/cmm-big@2x.png" alt="cmm-big@2x" className="w-[1106px] h-[465px] mx-auto" />
+        </div>
+        <div className="flex h-[633px] w-full gap-[20px] mt-[20px]">
+          <div className="bg-[#F4F0E4] rounded-[20px] w-full h-full">
+            <img src="/assets/coderunner@2x.png" alt="coderunner@2x" className="w-[580px] h-[400.52px]" />
+            <div className="flex flex-col ml-[60px]">
+              <img src="/assets/icon-coderunner.png" alt="icon-coderunner" className="w-[80px] h-[80px]" />
+              <span className="mt-[19px] font-semibold text-[26px] leading-[40.04px] tracking-normal text-[#26262B]">Write code</span>
+              <span className="font-normal text-lg leading-[32.04px] tracking-normal text-[#26262B]">Create applications in more than 25 languages</span>
+            </div>
+          </div>
+          <div className="bg-[#384C75] rounded-[20px] w-full h-full">
+            <div className="flex flex-col mt-[60px]">
+              <img src="/assets/icon-meeter.png" alt="icon-meeter" className="ml-[60px] w-[68px] h-[68px]" />
+              <span className="ml-[60px] mt-[19px] font-semibold text-[26px] leading-[40.04px] tracking-normal text-[#FFFFFF]">Join meetings in a click</span>
+              <span className="ml-[60px] font-normal text-lg leading-[32.04px] tracking-normal text-[#FFFFFF]">Quickly access links to your meetings from menu bar</span>
+              <img src="/assets/meeter@2x.png" alt="meeter@2x" className="mt-[52.46px] w-[580px] h-[360px]" />
+            </div>
+          </div>
+        </div>
+        <div className="mt-[100.57px] flex text-center mx-auto">
+          <span className="flex font-medium text-xl leading-[30px] tracking-[1.6px] gap-[13.31px] items-center cursor-pointer">
+            <img src="/assets/arrow.svg" alt="arrow" className="w-[27px] h-[21px]" />
+            View all superpowers
+            </span>
+        </div>
+        <div className="flex h-[67.84px] w-full mt-[102px] mb-[89px]">
+          <span className="w-1/2 font-semibold text-4xl leading-[51.84px] tracking-[1px]">Your Setapp journey.</span>
+          <span className="w-1/2 font-normal text-lg leading-[32.04px] tracking-normal">Type in your task into Setapp search and get instant app<br />recommendations.</span>
+        </div>
+      </div>
+      {/* End section2 */}
     </div>
   )
 }
